b307a42eac1067361878a6bc3f988dba703840e9
deeanalyst <deedexanalyst@gmail.com>
2025-08-21 15:21:30 +0200

    Revert "feat: remove whitelist from ERC20WhaleTrap"
    This reverts commit fd8f1207cf6197cc400a807727adbfb9e3f02d5a.

--- test/ERC20WhaleTrap.t.sol
+++ test/ERC20WhaleTrap.t.sol
@@ -2,96 +2,91 @@
 pragma solidity ^0.8.20;
 
 import {Test, console} from "forge-std/Test.sol";
-import {ERC20WhaleTrap} from "../src/ERC20WhaleTrap.sol";
+import {TestERC20WhaleTrap} from "./TestERC20WhaleTrap.sol";
 import {MockERC20} from "../src/mocks/MockERC20.sol";
 import {MockV3Aggregator} from "../src/mocks/MockV3Aggregator.sol";
+import {ERC20WhaleResponse} from "../src/ERC20WhaleResponse.sol";
 
 contract ERC20WhaleTrapTest is Test {
-    ERC20WhaleTrap public trap;
+    TestERC20WhaleTrap public trap;
     MockERC20 public token;
     MockV3Aggregator public priceFeed;
+    ERC20WhaleResponse public responseContract;
 
-    // --- Constants from ERC20WhaleTrap ---
-    address private constant TOKEN_ADDR = 0x26aeAb946Be6f4619dBBF88Fcc3C82C68506f9Ab;
-    address private constant PRICE_FEED_ADDR = 0x076BeCb937C163c6a963917CB970e2125B67a927;
-    address private constant TRACKED_WHALE = 0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B;
-    // ---
-
+    address internal constant TRACKED_WHALE = 0xF38eED066703d093B20Be0A9D9fcC8684F64cdc4;
     address internal constant OPERATOR = 0x0000000000000000000000000000000000000001;
 
     function setUp() public {
-        // Deploy mock contracts to get their bytecode
-        MockERC20 mockToken = new MockERC20("Mock Token", "MTK");
-        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(0); // Initial value doesn't matter here
+        token = new MockERC20("Mock Token", "MTK");
+        priceFeed = new MockV3Aggregator(2000e8); // Initial price $2000
+        responseContract = new ERC20WhaleResponse();
 
-        // Place the bytecode at the hardcoded addresses
-        vm.etch(TOKEN_ADDR, address(mockToken).code);
-        vm.etch(PRICE_FEED_ADDR, address(mockPriceFeed).code);
-
-        // Point our test contract's variables to those addresses
-        token = MockERC20(TOKEN_ADDR);
-        priceFeed = MockV3Aggregator(PRICE_FEED_ADDR);
-
-        // Manually initialize the state of the etched contract
-        priceFeed.setLatestAnswer(2000e8); // Initial price $2000
-
-        // Now we can instantiate the trap
-        trap = new ERC20WhaleTrap();
+        trap = new TestERC20WhaleTrap(TRACKED_WHALE, address(token), address(priceFeed));
+        trap.addToWhitelist(OPERATOR);
     }
 
     function test_Collect() public {
+        vm.prank(OPERATOR);
         bytes memory data = trap.collect();
         (address tracked, uint256 balance, int256 price) = abi.decode(data, (address, uint256, int256));
 
         assertEq(tracked, TRACKED_WHALE);
-        assertEq(balance, 0, "Initial balance should be 0");
-        assertEq(price, 2000e8, "Initial price should be 2000e8");
+        assertEq(balance, 0);
+        assertEq(price, 2000e8);
     }
 
     function test_ShouldRespond_NotTriggered_NoChange() public {
         bytes[] memory data = new bytes[](2);
+        vm.prank(OPERATOR);
         data[0] = trap.collect();
+        vm.prank(OPERATOR);
         data[1] = trap.collect();
 
         (bool should, ) = trap.shouldRespond(data);
-        assertFalse(should, "Should not respond when there is no change");
+        assertFalse(should);
     }
 
     function test_ShouldRespond_NotTriggered_BalanceChangeBelowThreshold() public {
         bytes[] memory data = new bytes[](2);
+        vm.prank(OPERATOR);
         data[0] = trap.collect();
 
         token.mint(TRACKED_WHALE, 500 * 1e18); // Below threshold
 
+        vm.prank(OPERATOR);
         data[1] = trap.collect();
 
         (bool should, ) = trap.shouldRespond(data);
-        assertFalse(should, "Should not respond for balance change below threshold");
+        assertFalse(should);
     }
 
     function test_ShouldRespond_NotTriggered_PriceChangeBelowThreshold() public {
         bytes[] memory data = new bytes[](2);
+        vm.prank(OPERATOR);
         data[0] = trap.collect();
 
-        priceFeed.setLatestAnswer(2040e8); // Below price threshold
+        priceFeed.setLatestAnswer(2020e8); // Below threshold
 
+        vm.prank(OPERATOR);
         data[1] = trap.collect();
 
         (bool should, ) = trap.shouldRespond(data);
-        assertFalse(should, "Should not respond for price change below threshold");
+        assertFalse(should);
     }
 
     function test_ShouldRespond_Triggered() public {
         bytes[] memory data = new bytes[](2);
+        vm.prank(OPERATOR);
         data[0] = trap.collect();
 
-        token.mint(TRACKED_WHALE, 1500 * 1e18); // Above balance threshold
-        priceFeed.setLatestAnswer(2100e8); // Above price threshold
+        token.mint(TRACKED_WHALE, 1500 * 1e18); // Above threshold
+        priceFeed.setLatestAnswer(2100e8); // Above threshold
 
+        vm.prank(OPERATOR);
         data[1] = trap.collect();
 
         (bool should, bytes memory responseData) = trap.shouldRespond(data);
-        assertTrue(should, "Should respond when both thresholds are crossed");
+        assertTrue(should);
 
         (address tracked, uint256 oldBalance, uint256 newBalance, int256 oldPrice, int256 newPrice) = abi.decode(responseData, (address, uint256, uint256, int256, int256));
 
@@ -100,5 +95,12 @@ contract ERC20WhaleTrapTest is Test {
         assertEq(newBalance, 1500 * 1e18);
         assertEq(oldPrice, 2000e8);
         assertEq(newPrice, 2100e8);
+    }
+
+    function test_Whitelist_RevertsIfNotWhitelisted() public {
+        address nonWhitelistedAddress = address(0xdeadbeef);
+        vm.prank(nonWhitelistedAddress);
+        vm.expectRevert("Not whitelisted");
+        trap.collect();
     }
 }
